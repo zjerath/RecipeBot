05b7be16716a3352128ec74950d2659087fdf8f1
jimin-kang <jimin.kang821@gmail.com>
2024-11-08 13:26:29 -0600

    basic step navigation implementation

--- handle_questions.py
+++ handle_questions.py
@@ -1,10 +1,23 @@
 import pandas as pd
 import json
+import re
 
 def build_google_search_query(question):
     return f"https://www.google.com/search?q={question.replace(' ', '+')}"
 
-def is_question(response):
+# detect general recipe questions, or how to
+# i.e. "show me the ingredients list", "how to preheat oven"
+def is_general_question(response):
+    return True
+
+# detect navigation requests 
+# i.e. "go to next step"
+def is_navigation_request(response):
+    return True
+
+# detect whether question is asking about parameters for a particular step
+# i.e. "what are the ingredients for this step"
+def is_step_specific_question(response):
     return True
 
 def return_ingredients(recipe):
@@ -24,10 +37,21 @@ def return_tools(recipe):
 def return_methods(recipe):
     methods_list = [f"{i+1}. {method}" for i, method in enumerate(recipe['methods'])]
     return f"Here are the methods used in {recipe['title']}:\n" + "\n".join(methods_list)
+    
+'''
+Currently handles:
+- recipe retrieval & display
+- simple "what is"
+- specific "how to"
 
+To do:
+- handle step navigation requests?
+- return step specific parameters?
+- handle both simultaneously?
+'''
 def handle_response(response, recipe):
     # Decide if the response is a question or a recipe flow statement
-    if is_question(response):
+    if is_general_question(response):
         # Decide if the question is about the recipe or about something else
         ingredient_words = ['ingredients']
         step_words = ['steps', 'instructions', 'make this', 'make it', 'cook this', 'cook it']
@@ -50,13 +74,84 @@ def handle_response(response, recipe):
             google_search_query = build_google_search_query(response)
             return google_search_query
     else:
-        # TODO: Handle statements 
+        # TODO: Handle navigation statements 
         return response
 
+
+def update_step(current_step, user_query, recipe_object):
+    '''
+    Given the current step and a user query, return the appropriate step to navigate to.
+    
+    Parameters:
+        current_step: index of the current step in the recipe
+        user_query: a user command specifying which step to navigate to
+                    the navigation request may be classified into the following navigation types:
+                        Current - stay at current step, typically used for repeat requests
+                        Previous - navigate to previous step
+                        Next - proceed to next step
+                        Nth - navigate to any arbitrary step in the recipe
+    '''
+
+    # TO DO: better logic for classifying requests, probably regex
+    navigation_keywords = {
+        'Current': ['repeat'],
+        'Previous': ['back', 'previous'],
+        'Next': ['next', 'proceed'],
+        'Nth': ['th step', 'st step']
+    }
+    
+    # categorize user_query
+    navigation_type = None
+    for type in navigation_keywords:
+        if any(i.lower() in user_query.lower() for i in navigation_keywords[type]):
+            navigation_type = type
+            break
+    
+    # return appropriate index corresponding to user_query category
+    match navigation_type:
+        case 'Current':
+            return current_step
+        case 'Previous':
+            assert current_step > 0, "there is no previous step"
+            return current_step - 1
+        case 'Next':
+            assert current_step < len(recipe_object['steps']), "there is no next step"
+            return current_step + 1
+        case 'Nth':
+            # TO DO: better step_num extraction logic
+            step_num = [int(s) for s in re.findall(r'\d+', user_query)][0] - 1
+            assert step_num >= 0 and step_num < len(recipe_object['steps']), "specified step is out of range, cannot navigate to invalid step"
+            return step_num
+
+'''
+To do/consider:
+- create separate Conversation class consisting of:
+    - recipe object
+    - question history
+    - current step
+    - response handling
+'''
 def main():
     recipe = json.load(open('example.json'))
     # print(recipe)
 
+    # Basic recipe navigation
+    current_step = 0
+    print(recipe["steps"][current_step]["text"] +'\n')
+
+    question_sequence = [
+        "Go to the next step",
+        "Please proceed",
+        "Go back",
+        "Repeat please",
+        "Go to the 5th step"
+    ]
+
+    for question in question_sequence:
+        current_step = update_step(current_step, question, recipe)
+        print(recipe["steps"][current_step]["text"] +'\n')
+    
+    # Basic general question answering
     question = "What is the recipe for lasagna?"
     print(question)
     # print(handle_response(question, recipe))
